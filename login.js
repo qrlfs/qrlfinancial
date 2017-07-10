0388afa7fed8a3b3281278b721a82b1ce647180b
qrlfs <dev@qrlfinancial.com>
2017-07-10 11:57:59 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#login_box form input").attr("onfocus", "activate_login();")
+  $("#login_box button[type='submit']").after("<a href='passwordhelp' class='pw_help glyphicon glyphicon-question-sign'></a>");
+});
+function activate_login() {
+  var login = $("#login_box");  
+  if (login.find('button[type="submit"]').length) {
+    //login is available
+    if ($("#login_box.login_active").length) {
+      //login is already active; do nothing
+    } else {
+      //configure the login animation
+      //dim background
+      $("body").prepend("<div id='login_active_bg' class='login_active' onclick='deactivate_login();'></div>")
+      //move login
+      $("#login_box").addClass("login_active");  
+      $("#login_user_name").focus();      
+    }    
+  } else {
+    //user is logged in; forward them to partner home
+    window.location = "home";
+  }
+}
+function deactivate_login() {
+  //remove animated login elements
+  $("#login_active_bg").remove();
+  $("#login_box").removeClass("login_active"); 
+}
